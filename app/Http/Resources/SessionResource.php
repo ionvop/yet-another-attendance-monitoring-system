34eac1839412d9dc240492144a01f148e0150f8a
ionvop <ionvop@gmail.com>
2026-08-14 14:40:02 +0800

    Update SessionResource.php

--- app/Http/Resources/SessionResource.php
+++ app/Http/Resources/SessionResource.php
@@ -14,7 +14,6 @@ class SessionResource extends JsonResource
             'event_id' => $this->event_id,
             'name' => $this->name,
             'start_time' => $this->start_time,
-            'end_time' => $this->end_time,
             'attendances_count' => $this->whenCounted('attendances'),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
